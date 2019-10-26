aca55736fc6c412b0b10ebbf9f17711afa33917a
Ville Vaarala <ville.l.vaarala@gmail.com>
2019-10-26 15:36:20 +0300

    Fetch projects list from API

--- src/views/portfolio/projects/Projects.vue
+++ src/views/portfolio/projects/Projects.vue
@@ -1,81 +1,154 @@
 <template>
   <div class="animated fadeIn">
-    <portfolio-widget-row/>
-    <BootstrapTable ref="table" :columns="columns" :data="data" :options="options"></BootstrapTable>
+    <portfolio-widget-row />
+    <BootstrapTable
+      ref="table"
+      :columns="columns"
+      :data="data"
+      :options="options"
+    ></BootstrapTable>
   </div>
 </template>
 
 <script>
-  import PortfolioWidgetRow from "../../dashboard/PortfolioWidgetRow";
+import axios from "axios";
+import api from "../../../shared/api";
+import PortfolioWidgetRow from "../../dashboard/PortfolioWidgetRow";
 
-  export default {
-    components: {
-      PortfolioWidgetRow
-    },
-    data () {
-      return {
-        columns: [
-          {
-            title: 'Project Name',
-            field: 'name'
-          },
-          {
-            title: 'Version',
-            field: 'version',
-          },
-          {
-            title: 'Last BOM Import',
-            field: 'lastImported',
-          },
-          {
-            title: 'BOM Format',
-            field: 'bomFormat',
-          },
-          {
-            title: 'Risk Score',
-            field: 'riskScore',
-          },
-          {
-            title: 'Active',
-            field: 'active',
-          },
-          {
-            title: 'Vulnerabilities',
-            field: 'vulnerabilities',
+export default {
+  components: {
+    PortfolioWidgetRow
+  },
+  data() {
+    return {
+      columns: [
+        {
+          title: "Project Name",
+          field: "name",
+          sortable: true
+        },
+        {
+          title: "Version",
+          field: "version",
+          sortable: true
+        },
+        {
+          title: "Last BOM Import",
+          field: "lastBomImport",
+          sortable: true,
+          formatter(timestamp, row, index) {
+            return typeof timestamp === "number"
+              ? new Date(timestamp).toDateString()
+              : "-";
           }
-        ],
-        data: [
-          {
-            id: 1,
-            name: 'Item 1',
-            price: '$1'
+        },
+        {
+          title: "BOM Format",
+          field: "lastBomImportFormat",
+          sortable: true
+        },
+        {
+          title: "Risk Score",
+          field: "metrics.inheritedRiskScore",
+          sortable: true
+        },
+        {
+          title: "Active",
+          field: "active",
+          formatter(value, row, index) {
+            return value === true ? '<i class="fa fa-check-square-o" />' : "";
           },
-          {
-            id: 2,
-            name: 'Item 2',
-            price: '$2'
-          },
-          {
-            id: 3,
-            name: 'Item 3',
-            price: '$3'
-          },
-          {
-            id: 4,
-            name: 'Item 4',
-            price: '$4'
-          },
-          {
-            id: 5,
-            name: 'Item 5',
-            price: '$5'
+          align: "center",
+          sortable: true
+        },
+        {
+          title: "Vulnerabilities",
+          field: "metrics",
+          sortable: true,
+          formatter(metrics, row, index) {
+            if (typeof metrics === "undefined") {
+              return "-"; // No vulnerability info available
+            }
+
+            if (metrics.vulnerabilities === 0) {
+              return '<span class="progress"><div class="progress-bar" style="width: 100%">0</div></span>';
+            }
+
+            return `
+            <span class="progress">
+              ${
+                metrics.critical > 0
+                  ? `<div class="progress-bar severity-critical-bg" style="width:${(100 *
+                      metrics.critical) /
+                      metrics.vulnerabilities}%">${metrics.critical}</div>`
+                  : ""
+              }
+              ${
+                metrics.high > 0
+                  ? `<div class="progress-bar severity-high-bg" style="width:${(100 *
+                      metrics.high) /
+                      metrics.vulnerabilities}%">${metrics.high}</div>`
+                  : ""
+              }
+              ${
+                metrics.medium > 0
+                  ? `<div class="progress-bar severity-medium-bg" style="width:${(100 *
+                      metrics.medium) /
+                      metrics.vulnerabilities}%">${metrics.medium}</div>`
+                  : ""
+              }
+              ${
+                metrics.low > 0
+                  ? `<div class="progress-bar severity-low-bg" style="width:${(100 *
+                      metrics.low) /
+                      metrics.vulnerabilities}%">${metrics.low}</div>`
+                  : ""
+              }
+            </span>
+            `;
           }
-        ],
-        options: {
-          search: true,
-          showColumns: true
+        }
+      ],
+      data: [],
+      options: {
+        search: true,
+        showColumns: true,
+        url: `${api.BASE_URL}/${api.URL_PROJECT}`,
+        ajaxOptions: {
+          beforeSend: function(xhr) {
+            xhr.setRequestHeader(
+              "Authorization",
+              `Bearer ${sessionStorage.getItem("token")}`
+            );
+          }
         }
       }
-    }
+    };
+    // mounted() {
+    //   axios
+    //     .get(`${api.BASE_URL}/${api.URL_PROJECT}`, {
+    //       headers: { Authorization: `Bearer ${sessionStorage.getItem("token")}` }
+    //     })
+    //     .then(result => {
+    //       this.data = result.data;
+    //       console.log(this.data);
+    //     });
+    // }
   }
+};
 </script>
+
+<style>
+.severity-critical-bg {
+  background: var(--severity-critical, red);
+}
+.severity-high-bg {
+  background: var(--severity-high, orange);
+}
+.severity-medium-bg {
+  background: var(--severity-medium, yellow);
+}
+.severity-low-bg {
+  background: var(--severity-low, green);
+}
+</style>
