b754cd0ab05e91ecdba7bbdcd46c6ac06f6758f7
Steve Springett <steve@springett.us>
2019-10-26 20:06:57 -0500

    lint correction

--- src/views/portfolio/projects/Projects.vue
+++ src/views/portfolio/projects/Projects.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script>
-import axios from "axios";
+//import axios from "axios";
 import api from "../../../shared/api";
 import PortfolioWidgetRow from "../../dashboard/PortfolioWidgetRow";
 
