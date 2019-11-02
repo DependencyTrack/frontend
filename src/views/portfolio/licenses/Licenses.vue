2beb33cb0a0b1f69f63d0182ec52a0bf7959ffe2
Steve Springett <steve@springett.us>
2019-11-01 23:52:57 -0500

    Added licenses

--- src/views/portfolio/licenses/Licenses.vue
+++ src/views/portfolio/licenses/Licenses.vue
@@ -1,15 +1,79 @@
 <template>
   <div class="animated fadeIn">
-    <portfolio-widget-row/>
+    <portfolio-widget-row />
+    <bootstrap-table
+      ref="table"
+      :columns="columns"
+      :data="data"
+      :options="options">
+    </bootstrap-table>
   </div>
 </template>
 
 <script>
+  import api from "../../../shared/api";
   import PortfolioWidgetRow from "../../dashboard/PortfolioWidgetRow";
+  import xssFilters from "xss-filters";
 
   export default {
     components: {
       PortfolioWidgetRow
+    },
+    data() {
+      return {
+        columns: [
+          {
+            title: this.$t('message.name'),
+            field: "name",
+            sortable: true
+          },
+          {
+            title: this.$t('message.spdx_license_id'),
+            field: "licenseId",
+            sortable: true,
+            formatter: function (value, row, index) {
+              let licenseurl = xssFilters.uriInUnQuotedAttr("../license/?licenseId=" + value);
+              return "<a href=\"" + licenseurl + "\">" + value + "</a>";
+            },
+          },
+          {
+            title: this.$t('message.osi_approved'),
+            field: "isOsiApproved",
+            sortable: false,
+            formatter: function (value, row, index) {
+              return value === true ? '<i class="fa fa-check-square-o" />' : "";
+            },
+          },
+          {
+            title: this.$t('message.fsf_libre'),
+            field: "isFsfLibre",
+            sortable: false,
+            formatter: function (value, row, index) {
+              return value === true ? '<i class="fa fa-check-square-o" />' : "";
+            },
+          }
+        ],
+        data: [],
+        options: {
+          search: true,
+          showColumns: true,
+          showRefresh: true,
+          pagination: true,
+          silentSort: false,
+          sidePagination: 'server',
+          queryParamsType: 'pageSize',
+          pageList: '[10, 25, 50, 100]',
+          pageSize: 10,
+          icons: {
+            refresh: 'fa-refresh'
+          },
+          responseHandler: function (res, xhr) {
+            res.total = xhr.getResponseHeader("X-Total-Count");
+            return res;
+          },
+          url: `${api.BASE_URL}/${api.URL_LICENSE}`
+        }
+      };
     }
-  }
+  };
 </script>
