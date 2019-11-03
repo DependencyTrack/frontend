7fe445343dc42f0f075bba12a41c439987b801f7
Steve Springett <steve@springett.us>
2019-11-02 23:23:11 -0500

    Portfolio vuln chart now working with real data

--- src/views/dashboard/ChartPortfolioVulnerabilities.vue
+++ src/views/dashboard/ChartPortfolioVulnerabilities.vue
@@ -1,100 +1,139 @@
 <script>
-import { Line } from 'vue-chartjs'
-import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities'
-import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips'
-import { random } from '@/shared/utils'
+  import api from "../../shared/api";
+  import common from "../../shared/common"
+  import { Line } from 'vue-chartjs'
+  import { getStyle } from '@coreui/coreui/dist/js/coreui-utilities'
+  import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips'
 
-export default {
-  extends: Line,
-  props: ['height'],
-  mounted () {
-    const brandSuccess = getStyle('--success') || '#4dbd74';
-    const brandInfo = getStyle('--info') || '#20a8d8';
-    const brandDanger = getStyle('--danger') || '#f86c6b';
+  export default {
+    extends: Line,
+    props: ['height'],
+    methods: {
+      render: function(metrics) {
+        const criticalStyle = getStyle('--severity-critical');
+        const highStyle = getStyle('--severity-high');
+        const mediumStyle = getStyle('--severity-medium');
+        const lowStyle = getStyle('--severity-low');
+        const unassignedStyle = getStyle('--severity-unassigned');
 
-    let elements = 27;
-    const data1 = [];
-    const data2 = [];
-    const data3 = [];
+        let labels = [];
+        let criticalData = [];
+        let highData = [];
+        let mediumData = [];
+        let lowData = [];
+        let unassignedData = [];
 
-    for (let i = 0; i <= elements; i++) {
-      data1.push(random(50, 200));
-      data2.push(random(80, 100));
-      data3.push(65);
-    }
-    this.renderChart({
-      labels: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
-      datasets: [
-        {
-          label: 'My First dataset',
-          backgroundColor: hexToRgba(brandInfo, 10),
-          borderColor: brandInfo,
-          pointHoverBackgroundColor: '#fff',
-          borderWidth: 2,
-          data: data1
-        },
-        {
-          label: 'My Second dataset',
-          backgroundColor: 'transparent',
-          borderColor: brandSuccess,
-          pointHoverBackgroundColor: '#fff',
-          borderWidth: 2,
-          data: data2
-        },
-        {
-          label: 'My Third dataset',
-          backgroundColor: 'transparent',
-          borderColor: brandDanger,
-          pointHoverBackgroundColor: '#fff',
-          borderWidth: 1,
-          borderDash: [8, 5],
-          data: data3
-        }
-      ]
-    }, {
-      tooltips: {
-        enabled: false,
-        custom: CustomTooltips,
-        intersect: true,
-        mode: 'index',
-        position: 'nearest',
-        callbacks: {
-          labelColor: function (tooltipItem, chart) {
-            return { backgroundColor: chart.data.datasets[tooltipItem.datasetIndex].borderColor }
+        for (let i = 0; i < metrics.length; i++) {
+          labels.push(common.formatTimestamp(metrics[i].firstOccurrence));
+          criticalData.push(metrics[i].critical);
+          highData.push(metrics[i].high);
+          mediumData.push(metrics[i].medium);
+          lowData.push(metrics[i].low);
+          unassignedData.push(metrics[i].unassigned);
+
+          if (i === metrics.length - 1) {
+            labels.push(common.formatTimestamp(metrics[i].lastOccurrence));
+            criticalData.push(metrics[i].critical);
+            highData.push(metrics[i].high);
+            mediumData.push(metrics[i].medium);
+            lowData.push(metrics[i].low);
+            unassignedData.push(metrics[i].unassigned);
           }
         }
-      },
-      maintainAspectRatio: false,
-      legend: {
-        display: false
-      },
-      scales: {
-        xAxes: [{
-          gridLines: {
-            drawOnChartArea: false
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            beginAtZero: true,
-            maxTicksLimit: 5,
-            stepSize: Math.ceil(250 / 5),
-            max: 250
+
+        this.renderChart({
+          labels: labels,
+          datasets: [
+            {
+              label: this.$t('severity.critical'),
+              backgroundColor: 'transparent',
+              borderColor: criticalStyle,
+              pointHoverBackgroundColor: '#fff',
+              data: criticalData
+            },
+            {
+              label: this.$t('severity.high'),
+              backgroundColor: 'transparent',
+              borderColor: highStyle,
+              pointHoverBackgroundColor: '#fff',
+              data: highData
+            },
+            {
+              label: this.$t('severity.medium'),
+              backgroundColor: 'transparent',
+              borderColor: mediumStyle,
+              pointHoverBackgroundColor: '#fff',
+              data: mediumData
+            },
+            {
+              label: this.$t('severity.low'),
+              backgroundColor: 'transparent',
+              borderColor: lowStyle,
+              pointHoverBackgroundColor: '#fff',
+              data: lowData
+            },
+            {
+              label: this.$t('severity.unassigned'),
+              backgroundColor: 'transparent',
+              borderColor: unassignedStyle,
+              pointHoverBackgroundColor: '#fff',
+              data: unassignedData
+            }
+          ]
+        }, {
+          tooltips: {
+            enabled: false,
+            custom: CustomTooltips,
+            intersect: true,
+            mode: 'index',
+            position: 'nearest',
+            callbacks: {
+              labelColor: function (tooltipItem, chart) {
+                return { backgroundColor: chart.data.datasets[tooltipItem.datasetIndex].borderColor }
+              }
+            }
           },
-          gridLines: {
-            display: true
+          maintainAspectRatio: false,
+          legend: {
+            display: false
+          },
+          scales: {
+            xAxes: [{
+              gridLines: {
+                drawOnChartArea: false
+              }
+            }],
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+                maxTicksLimit: 1
+              },
+              gridLines: {
+                display: true
+              }
+            }]
+          },
+          elements: {
+            line: {
+              tension: 0.1,
+              borderWidth: 2
+            },
+            point: {
+              radius: 0,
+              hitRadius: 20,
+              hoverRadius: 4,
+              hoverBorderWidth: 3
+            }
           }
-        }]
-      },
-      elements: {
-        point: {
-          radius: 0,
-          hitRadius: 10,
-          hoverRadius: 4,
-          hoverBorderWidth: 3
-        }
+        })
       }
-    })
+    },
+    mounted () {
+      const daysBack = 90;
+      let url = `${api.BASE_URL}/${api.URL_METRICS}/portfolio/${daysBack}/days`;
+      this.axios.get(url).then((response) => {
+        this.render(response.data);
+      });
+    }
   }
-}
 </script>
