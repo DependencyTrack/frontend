da37c0a06baf60b2ae5ce9bedaac929a618d2130
Steve Springett <steve@springett.us>
2019-11-06 21:23:16 -0600

    Added placeholder

---
+++ src/views/portfolio/projects/Project.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="animated fadeIn">
+    <portfolio-widget-row />
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import api from "../../../shared/api";
+  import PortfolioWidgetRow from "../../dashboard/PortfolioWidgetRow";
+  import SeverityProgressBar from "../../components/SeverityProgressBar";
+  import xssFilters from "xss-filters";
+
+  export default {
+    components: {
+      PortfolioWidgetRow
+    }
+  };
+</script>
