a132154bdd79b2630e16d3474c7f7e5b8336aab4
Steve Springett <steve@springett.us>
2019-11-06 20:59:45 -0600

    Moved static images into assets to fix pathing issue related to unknown context deployment

--- src/containers/DefaultHeader.vue
+++ src/containers/DefaultHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <AppHeader fixed>
     <SidebarToggler class="d-lg-none" display="md" mobile />
-    <img class="d-lg-inline d-md-none d-sm-none bv-d-xs-down-none" style="margin-left:10px" src="img/brand/logo-symbol-greyscale.svg" width="30" height="30" alt="Dependency-Track Logo"/>
+    <img class="d-lg-inline d-md-none d-sm-none bv-d-xs-down-none" style="margin-left:10px" src="@/assets/img/brand/logo-symbol-greyscale.svg" width="30" height="30" alt="Dependency-Track Logo"/>
     <SidebarToggler class="d-md-down-none" display="lg" :defaultOpen=true />
     <b-navbar-nav class="ml-auto">
       <DefaultHeaderProfileDropdown/>
