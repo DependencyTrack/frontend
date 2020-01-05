b40c6fd96ae7174184e1b93cdf179ee2500a26f8
Steve Springett <steve@springett.us>
2020-01-04 22:42:32 -0600

    Added support for affected projects. Related: https://github.com/DependencyTrack/dependency-track/pull/359

---
+++ src/views/portfolio/vulnerabilities/AffectedProjects.vue
@@ -0,0 +1,57 @@
+<template>
+    <bootstrap-table
+      ref="table"
+      :columns="columns"
+      :data="data"
+      :options="options">
+    </bootstrap-table>
+</template>
+
+<script>
+  import xssFilters from "xss-filters";
+  import permissionsMixin from "../../../mixins/permissionsMixin";
+
+  export default {
+    mixins: [permissionsMixin],
+    props: {
+      source: String,
+      vulnId: String
+    },
+    data() {
+      return {
+        columns: [
+          {
+            title: this.$t('message.name'),
+            field: "name",
+            sortable: true,
+            formatter(value, row, index) {
+              let url = xssFilters.uriInUnQuotedAttr("../../projects/" + row.uuid);
+              return `<a href="${url}">${xssFilters.inHTMLData(value)}</a>`;
+            }
+          },
+          {
+            title: this.$t('message.version'),
+            field: "version",
+            sortable: true,
+          }
+        ],
+        data: [],
+        options: {
+          search: true,
+          showColumns: true,
+          showRefresh: true,
+          pagination: true,
+          silentSort: false,
+          sidePagination: 'client',
+          queryParamsType: 'pageSize',
+          pageList: '[10, 25, 50, 100]',
+          pageSize: 10,
+          icons: {
+            refresh: 'fa-refresh'
+          },
+          url: `${this.$api.BASE_URL}/${this.$api.URL_VULNERABILITY}/source/${this.source}/vuln/${this.vulnId}/projects`
+        }
+      };
+    }
+  };
+</script>
