208e9c39e84c729b7c20ddf784a15ceeaf0ad800
Steve Springett <steve@springett.us>
2020-01-12 22:45:47 -0600

    Restructured admin menu and added placeholders for each section

--- src/views/administration/AdminMenu.vue
+++ src/views/administration/AdminMenu.vue
@@ -1,87 +1,204 @@
 <template>
   <div>
+
     <b-card no-body class="admin-menu">
       <div slot="header" v-b-toggle.configurationMenu>
         <i class="fa fa-align-justify"></i><strong>&nbsp;&nbsp;{{ $t('admin.configuration') }}</strong>
       </div>
       <b-collapse id="configurationMenu" visible accordion="admin-accordion" role="tabpanel">
-        <b-list-group flush>
-          <b-list-group-item href="#">{{ $t('admin.general') }}</b-list-group-item>
-          <b-list-group-item href="#">{{ $t('admin.bom_formats') }}</b-list-group-item>
-          <b-list-group-item href="#">{{ $t('admin.email') }}</b-list-group-item>
-          <b-list-group-item href="#">{{ $t('admin.internal_components') }}</b-list-group-item>
-        </b-list-group>
+        <div class="list-group" id="list-tab" role="tablist">
+          <a v-for="item in configurationMenu" class="list-group-item list-group-item-action" data-toggle="list" role="tab"
+             :href="item.href">{{ item.name }}</a>
+        </div>
       </b-collapse>
     </b-card>
+
     <b-card no-body class="admin-menu">
       <div slot="header" v-b-toggle.analyzersMenu>
         <i class="fa fa-align-justify"></i><strong>&nbsp;&nbsp;{{ $t('admin.analyzers') }}</strong>
       </div>
       <b-collapse id="analyzersMenu" accordion="admin-accordion" role="tabpanel">
-        <b-list-group flush>
-          <b-list-group-item href="#">{{ $t('admin.internal_analyzer') }}</b-list-group-item>
-          <b-list-group-item href="#">{{ $t('admin.npm_audit') }}</b-list-group-item>
-          <b-list-group-item href="#">{{ $t('admin.oss_index') }}</b-list-group-item>
-          <b-list-group-item href="#">{{ $t('admin.vulndb') }}</b-list-group-item>
-        </b-list-group>
+        <div class="list-group" id="list-tab" role="tablist">
+          <a v-for="item in analyzersMenu" class="list-group-item list-group-item-action" data-toggle="list" role="tab"
+             :href="item.href">{{ item.name }}</a>
+        </div>
       </b-collapse>
     </b-card>
+
     <b-card no-body class="admin-menu">
       <div slot="header" v-b-toggle.repositoriesMenu>
         <i class="fa fa-align-justify"></i><strong>&nbsp;&nbsp;{{ $t('admin.repositories') }}</strong>
       </div>
       <b-collapse id="repositoriesMenu" accordion="admin-accordion" role="tabpanel">
-        <b-list-group flush>
-          <b-list-group-item href="#">{{ $t('admin.gem') }}</b-list-group-item>
-          <b-list-group-item href="#">{{ $t('admin.hex') }}</b-list-group-item>
-          <b-list-group-item href="#">{{ $t('admin.maven') }}</b-list-group-item>
-          <b-list-group-item href="#">{{ $t('admin.npm') }}</b-list-group-item>
-          <b-list-group-item href="#">{{ $t('admin.nuget') }}</b-list-group-item>
-          <b-list-group-item href="#">{{ $t('admin.python') }}</b-list-group-item>
-        </b-list-group>
+        <div class="list-group" id="list-tab" role="tablist">
+          <a v-for="item in repositoriesMenu" class="list-group-item list-group-item-action" data-toggle="list" role="tab"
+             :href="item.href">{{ item.name }}</a>
+        </div>
       </b-collapse>
     </b-card>
+
     <b-card no-body class="admin-menu">
       <div slot="header" v-b-toggle.notificationsMenu>
         <i class="fa fa-align-justify"></i><strong>&nbsp;&nbsp;{{ $t('admin.notifications') }}</strong>
       </div>
       <b-collapse id="notificationsMenu" accordion="admin-accordion" role="tabpanel">
-        <b-list-group flush>
-          <b-list-group-item href="#">{{ $t('admin.alerts') }}</b-list-group-item>
-          <b-list-group-item href="#">{{ $t('admin.templates') }}</b-list-group-item>
-        </b-list-group>
+        <div class="list-group" id="list-tab" role="tablist">
+          <a v-for="item in notificationsMenu" class="list-group-item list-group-item-action" data-toggle="list" role="tab"
+             :href="item.href">{{ item.name }}</a>
+        </div>
       </b-collapse>
     </b-card>
+
     <b-card no-body class="admin-menu">
       <div slot="header" v-b-toggle.integrationsMenu>
         <i class="fa fa-align-justify"></i><strong>&nbsp;&nbsp;{{ $t('admin.integrations') }}</strong>
       </div>
       <b-collapse id="integrationsMenu" accordion="admin-accordion" role="tabpanel">
-        <b-list-group flush>
-          <b-list-group-item href="#">{{ $t('admin.fortify_ssc') }}</b-list-group-item>
-          <b-list-group-item href="#">{{ $t('admin.kenna_security') }}</b-list-group-item>
-          <b-list-group-item href="#">{{ $t('admin.threadfix') }}</b-list-group-item>
-        </b-list-group>
+        <div class="list-group" id="list-tab" role="tablist">
+          <a v-for="item in integrationsMenu" class="list-group-item list-group-item-action" data-toggle="list" role="tab"
+             :href="item.href">{{ item.name }}</a>
+        </div>
       </b-collapse>
     </b-card>
+
     <b-card no-body class="admin-menu">
       <div slot="header" v-b-toggle.accessManagementMenu>
         <i class="fa fa-align-justify"></i><strong>&nbsp;&nbsp;{{ $t('admin.access_management') }}</strong>
       </div>
       <b-collapse id="accessManagementMenu" accordion="admin-accordion" role="tabpanel">
-        <b-list-group flush>
-          <b-list-group-item href="#">{{ $t('admin.ldap_users') }}</b-list-group-item>
-          <b-list-group-item href="#">{{ $t('admin.managed_users') }}</b-list-group-item>
-          <b-list-group-item href="#">{{ $t('admin.teams') }}</b-list-group-item>
-          <b-list-group-item href="#">{{ $t('admin.permissions') }}</b-list-group-item>
-        </b-list-group>
+        <div class="list-group" id="list-tab" role="tablist">
+          <a v-for="item in accessManagementMenu" class="list-group-item list-group-item-action" data-toggle="list" role="tab"
+             :href="item.href">{{ item.name }}</a>
+        </div>
       </b-collapse>
     </b-card>
+
   </div>
 </template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+        configurationMenu: [
+          {
+            name: this.$t('admin.general'),
+            href: "#generalConfigTab"
+          },
+          {
+            name: this.$t('admin.bom_formats'),
+            href: "#artifactsTab"
+          },
+          {
+            name: this.$t('admin.email'),
+            href: "#emailTab"
+          },
+          {
+            name: this.$t('admin.internal_components'),
+            href: "#internalComponentsTab"
+          }
+        ],
+        analyzersMenu: [
+          {
+            name: this.$t('admin.internal_analyzer'),
+            href: "#scannerInternalTab"
+          },
+          {
+            name: this.$t('admin.npm_audit'),
+            href: "#scannerNpmAuditTab"
+          },
+          {
+            name: this.$t('admin.oss_index'),
+            href: "#scannerOssIndexTab"
+          },
+          {
+            name: this.$t('admin.vulndb'),
+            href: "#scannerVulnDbTab"
+          }
+        ],
+        repositoriesMenu: [
+          {
+            name: this.$t('admin.gem'),
+            href: "#repositoryGemTab"
+          },
+          {
+            name: this.$t('admin.hex'),
+            href: "#repositoryHexTab"
+          },
+          {
+            name: this.$t('admin.maven'),
+            href: "#repositoryMavenTab"
+          },
+          {
+            name: this.$t('admin.npm'),
+            href: "#repositoryNpmTab"
+          },
+          {
+            name: this.$t('admin.nuget'),
+            href: "#repositoryNugetTab"
+          },
+          {
+            name: this.$t('admin.python'),
+            href: "#repositoryPythonTab"
+          }
+        ],
+        notificationsMenu: [
+          {
+            name: this.$t('admin.alerts'),
+            href: "#notificationAlertTab"
+          },
+          {
+            name: this.$t('admin.templates'),
+            href: "#notificationTemplateTab"
+          }
+        ],
+        integrationsMenu: [
+          {
+            name: this.$t('admin.fortify_ssc'),
+            href: "#integrationsFortifySscTab"
+          },
+          {
+            name: this.$t('admin.kenna_security'),
+            href: "#integrationsKennaSecurityTab"
+          },
+          {
+            name: this.$t('admin.threadfix'),
+            href: "#integrationsThreadFixTab"
+          }
+        ],
+        accessManagementMenu: [
+          {
+            name: this.$t('admin.ldap_users'),
+            href: "#ldapUsersTab"
+          },
+          {
+            name: this.$t('admin.managed_users'),
+            href: "#managedUsersTab"
+          },
+          {
+            name: this.$t('admin.teams'),
+            href: "#teamsTab"
+          },
+          {
+            name: this.$t('admin.permissions'),
+            href: "#permissionsTab"
+          }
+        ]
+      }
+    }
+  }
+</script>
 
 <style scoped>
   .admin-menu {
     margin-bottom: .3rem;
+  }
+  .list-group-item:first-child, .list-group-item:last-child {
+    border-radius: 0;
+  }
+  .list-group-item {
+    border-left: 0;
+    border-right: 0;
   }
 </style>
--- src/views/administration/Administration.vue
+++ src/views/administration/Administration.vue
@@ -6,7 +6,106 @@
         <admin-menu />
       </b-col>
       <b-col xs="6" sm="8" md="10">
-        Content here
+        <div class="tab-content">
+          <!-- CONFIGURATION -->
+          <b-card :header="$t('admin.general')" class="tab-pane active" id="generalConfigTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card :header="$t('admin.bom_formats')" class="tab-pane" id="artifactsTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card :header="$t('admin.email')" class="tab-pane" id="emailTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card :header="$t('admin.internal_components')" class="tab-pane" id="internalComponentsTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <!-- ANALYZERS -->
+          <b-card :header="$t('admin.internal_analyzer') " class="tab-pane" id="scannerInternalTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card :header="$t('admin.npm_audit')" class="tab-pane" id="scannerNpmAuditTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card :header="$t('admin.oss_index')" class="tab-pane" id="scannerOssIndexTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card :header="$t('admin.vulndb')" class="tab-pane" id="scannerVulnDbTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <!-- REPOSITORIES -->
+          <b-card :header="$t('admin.gem') " class="tab-pane" id="repositoryGemTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card :header="$t('admin.hex')" class="tab-pane" id="repositoryHexTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card :header="$t('admin.maven')" class="tab-pane" id="repositoryMavenTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card :header="$t('admin.npm')" class="tab-pane" id="repositoryNpmTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card :header="$t('admin.nuget')" class="tab-pane" id="repositoryNugetTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card :header="$t('admin.python')" class="tab-pane" id="repositoryPythonTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <!-- NOTIFICATIONS -->
+          <b-card :header="$t('admin.alerts') " class="tab-pane" id="notificationAlertTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card :header="$t('admin.templates')" class="tab-pane" id="notificationTemplateTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <!-- INTEGRATIONS -->
+          <b-card :header="$t('admin.fortify_ssc')" class="tab-pane" id="integrationsFortifySscTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card :header="$t('admin.kenna_security')" class="tab-pane" id="integrationsKennaSecurityTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card :header="$t('admin.threadfix')" class="tab-pane" id="integrationsThreadFixTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <!-- ACCESS MANAGEMENT -->
+          <b-card :header="$t('admin.ldap_users')" class="tab-pane" id="ldapUsersTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card :header="$t('admin.managed_users')" class="tab-pane" id="managedUsersTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card :header="$t('admin.teams')" class="tab-pane" id="teamsTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card :header="$t('admin.permissions')" class="tab-pane" id="permissionsTab" role="tabpanel">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+        </div>
       </b-col>
     </b-row>
   </div>
@@ -20,4 +119,16 @@
     }
   }
 </script>
+
+<style>
+  .tab-content {
+    border: 0;
+  }
+  .tab-content .tab-pane {
+    padding: 0;
+  }
+  .card-header {
+    font-weight: 700;
+  }
+</style>
 
