cad817379d5a0bf34c14b2a24296c310e5fdfcce
Steve Springett <steve@springett.us>
2020-01-19 00:39:50 -0600

    Added internal analyzer config

--- src/views/administration/analyzers/InternalAnalyzer.vue
+++ src/views/administration/analyzers/InternalAnalyzer.vue
@@ -1,7 +1,13 @@
 <template>
   <b-card no-body :header="header">
     <b-card-body>
-      {{ header }}
+      <c-switch id="scannerEnabled" color="primary" v-model="scannerEnabled" label v-bind="labelIcon" />{{$t('admin.analyzer_internal_enable')}}
+      <!--
+      <br/>
+      <c-switch id="scannerCpeFuzzyEnableInput" color="primary" v-model="scannerCpeFuzzyEnableInput" label v-bind="labelIcon" />{{$t('admin.analyzer_internal_fuzzy_enable')}}
+      <br/>
+      <c-switch id="scannerCpeFuzzyExcludePurlInput" color="primary" v-model="scannerCpeFuzzyExcludePurlInput" label v-bind="labelIcon" />{{$t('admin.analyzer_internal_fuzzy_exclude_purl')}}
+      -->
     </b-card-body>
     <b-card-footer>
       <b-button variant="outline-primary" class="px-4" @click="saveChanges">{{ $t('message.update') }}</b-button>
@@ -10,9 +16,54 @@
 </template>
 
 <script>
+  import { Switch as cSwitch } from '@coreui/vue';
+  import common from "../../../shared/common";
+  import configPropertyMixin from "../mixins/configPropertyMixin";
+
   export default {
+    mixins: [configPropertyMixin],
     props: {
       header: String
+    },
+    components: {
+      cSwitch
+    },
+    data() {
+      return {
+        scannerEnabled: false,
+        scannerCpeFuzzyEnableInput: false,
+        scannerCpeFuzzyExcludePurlInput: false,
+        labelIcon: {
+          dataOn: '\u2713',
+          dataOff: '\u2715'
+        },
+      }
+    },
+    methods: {
+      saveChanges: function() {
+        this.updateConfigProperties([
+          {groupName: 'scanner', propertyName: 'internal.enabled', propertyValue: this.scannerEnabled}
+          // TODO: Future
+          // {groupName: 'scanner', propertyName: 'internal.fuzzy.enabled', propertyValue: this.fuzzyEnabled}
+          // {groupName: 'scanner', propertyName: 'internal.fuzzy.exclude.purl', propertyValue: this.fuzzyExcludePurl}
+        ]);
+      }
+    },
+    created () {
+      this.axios.get(this.configUrl).then((response) => {
+        let configItems = response.data.filter(function (item) { return item.groupName === "scanner" });
+        for (let i=0; i<configItems.length; i++) {
+          let item = configItems[i];
+          switch (item.propertyName) {
+            case "internal.enabled":
+              this.scannerEnabled = common.toBoolean(item.propertyValue); break;
+            case "internal.fuzzy.enabled":
+              this.scannerCpeFuzzyEnableInput = common.toBoolean(item.propertyValue); break;
+            case "internal.fuzzy.exclude.purl":
+              this.scannerCpeFuzzyExcludePurlInput = common.toBoolean(item.propertyValue); break;
+          }
+        }
+      });
     }
   }
 </script>
