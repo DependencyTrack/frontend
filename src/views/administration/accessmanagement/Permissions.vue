0c39dfc7115523baa5caec6844278d18a527a954
Steve Springett <steve@springett.us>
2020-01-19 22:52:05 -0600

    Added permissions

--- src/views/administration/accessmanagement/Permissions.vue
+++ src/views/administration/accessmanagement/Permissions.vue
@@ -1,15 +1,66 @@
 <template>
   <b-card no-body :header="header">
     <b-card-body>
-      {{ header }}
+      <bootstrap-table
+        ref="table"
+        :columns="columns"
+        :data="data"
+        :options="options">
+      </bootstrap-table>
     </b-card-body>
   </b-card>
 </template>
 
 <script>
+  import xssFilters from "xss-filters";
+  import common from "../../../shared/common";
+
   export default {
     props: {
       header: String
+    },
+    data() {
+      return {
+        columns: [
+          {
+            title: this.$t('message.name'),
+            field: "name",
+            sortable: true,
+            formatter(value, row, index) {
+              return xssFilters.inHTMLData(common.valueWithDefault(value, ""));
+            }
+          },
+          {
+            title: this.$t('message.description'),
+            field: "description",
+            sortable: false,
+            formatter(value, row, index) {
+              return xssFilters.inHTMLData(common.valueWithDefault(value, ""));
+            }
+          }
+        ],
+        data: [],
+        options: {
+          search: true,
+          showColumns: true,
+          showRefresh: true,
+          pagination: true,
+          silentSort: false,
+          sidePagination: 'client',
+          queryParamsType: 'pageSize',
+          pageList: '[10, 25, 50, 100]',
+          pageSize: 10,
+          icons: {
+            refresh: 'fa-refresh'
+          },
+          toolbar: '#projectsToolbar',
+          responseHandler: function (res, xhr) {
+            res.total = xhr.getResponseHeader("X-Total-Count");
+            return res;
+          },
+          url: `${this.$api.BASE_URL}/${this.$api.URL_PERMISSION}`
+        }
+      };
     }
   }
 </script>
