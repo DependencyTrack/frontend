553049c960c7fcfdfc83d4ff902929322088da39
Steve Springett <steve@springett.us>
2020-02-20 19:02:54 -0800

    Allowing cross tab login and synchronizing logout across tabs. Ported from v3.7

--- src/containers/DefaultHeaderProfileDropdown.vue
+++ src/containers/DefaultHeaderProfileDropdown.vue
@@ -9,10 +9,10 @@
         class="text-center">
         <strong>Settings</strong>
       </b-dropdown-header>
-      <b-dropdown-item><i class="fa fa-user" /> Profile</b-dropdown-item>
-      <b-dropdown-item><i class="fa fa-wrench" /> Settings</b-dropdown-item>
+      <b-dropdown-item><i class="fa fa-user text-primary" /> Profile</b-dropdown-item>
+      <b-dropdown-item><i class="fa fa-wrench text-primary" /> Settings</b-dropdown-item>
       <b-dropdown-divider />
-      <b-dropdown-item @click="logout"><i class="fa fa-sign-out" /> Logout</b-dropdown-item>
+      <b-dropdown-item @click="logout"><i class="fa fa-sign-out text-primary" /> Logout</b-dropdown-item>
     </template>
   </AppHeaderDropdown>
 </template>
@@ -29,6 +29,10 @@
     },
     methods: {
       logout: function () {
+        // Instructs all tabs (via localStorage event) that the session is being invalidated
+        localStorage.setItem("sessionInvalidate", Date.now().toLocaleString());
+        localStorage.removeItem("sessionInvalidate");
+        // Removes the token from session storage and reload
         sessionStorage.removeItem('token');
         this.$router.replace({ name: "Login" });
       }
