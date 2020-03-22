1cf115b959f9c8bb03da712a16eaa03529f9639a
nscuro <nscuro@protonmail.com>
2020-03-22 18:10:45 +0100

    Automatically copy oidc-client.js.min from node_modules during build

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,18 @@
+const CopyPlugin = require('copy-webpack-plugin')
+
 module.exports = {
   lintOnSave: false,
   runtimeCompiler: true,
   //publicPath: "/",
   publicPath: process.env.NODE_ENV === 'production' ? '.' : '/',
   devServer: {
-    proxy: { "/api": { target: process.env.VUE_APP_SERVER_URL } }
+    proxy: { "/api": { target: "http://localhost:8080" } }
+  },
+  configureWebpack: {
+    plugins: [
+      new CopyPlugin([
+        { from: "node_modules/oidc-client/dist/oidc-client.min.js", to: "static/js", force: true }
+      ])
+    ]
   }
 };
