2ee4c56f7d8bfbd317d0f93a2fa7bac9b38612aa
nscuro <nscuro@protonmail.com>
2020-07-29 22:29:08 +0200

    Minor adjustments

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,12 +2,23 @@
 
 set -e
 
+if [ -z "$API_BASE_URL" ]; then
+    echo "Mandatory \$API_BASE_URL not set"
+    exit 1
+fi
+
+# Apply ENV vars to temporary config.json
 jq  --arg apiBaseUrl "$API_BASE_URL" \
     --arg oidcIssuer "$OIDC_ISSUER" \
     --arg oidcClientId "$OIDC_CLIENT_ID" \
     --arg oidcFlow "$OIDC_FLOW" \
-    '.API_BASE_URL = $apiBaseUrl | .OIDC_ISSUER = $oidcIssuer | .OIDC_CLIENT_ID = $oidcClientId | .OIDC_FLOW = $oidcFlow' \
-    ./static/config.json > ./static/config.json.new
-mv -f ./static/config.json.new ./static/config.json
+    '.API_BASE_URL = $apiBaseUrl 
+        | .OIDC_ISSUER = $oidcIssuer 
+        | .OIDC_CLIENT_ID = $oidcClientId 
+        | .OIDC_FLOW = $oidcFlow' \
+    ./static/config.json > /tmp/config.json
+
+# Override default config file
+mv -f /tmp/config.json ./static/config.json
 
 exec "$@"
