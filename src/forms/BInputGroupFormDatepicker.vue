3c84cc4b02e34f16ed73a54440d9a4560f5cfe6b
Steve Springett <steve@springett.us>
2022-03-25 16:46:28 -0500

    Initial commit for adding vulnerabilities to the system - https://github.com/DependencyTrack/dependency-track/issues/96

---
+++ src/forms/BInputGroupFormDatepicker.vue
@@ -0,0 +1,118 @@
+<template>
+  <b-form-group :id="id" :label="label" :label-for="`${id}-input`" :label-class="labelClasses">
+    <b-input-group :class="inputGroupSize">
+      <b-input-group-prepend v-if="icon"><b-input-group-text><i :class="icon"></i></b-input-group-text></b-input-group-prepend>
+      <b-form-datepicker :id="`${id}-input`"
+                         :class="inputClasses"
+                         v-model="innerValue"
+                         :placeholder="label"
+                         :state="feedbackState()"
+                         :autocomplete=autocomplete
+                         :required=isRequired
+                         :readonly=readonly
+                         :disabled=isDisabled
+                         v-on="inputListeners"
+                         :date-format-options="{ year: 'numeric', month: '2-digit', day: '2-digit' }"
+                         v-on:blur="hadFocus = true"/>
+      <b-input-group-append v-if="tooltip"><b-input-group-text v-b-tooltip.hover :title="tooltip"><i class="cui-info font-lg"></i></b-input-group-text></b-input-group-append>
+    </b-input-group>
+    <b-form-invalid-feedback v-if="this.feedback === 'true'" :state="feedbackState()">
+      {{ feedbackText }}
+    </b-form-invalid-feedback>
+  </b-form-group>
+</template>
+
+<script>
+import common from "../shared/common";
+
+export default {
+  props: {
+    id: String,
+    label: String,
+    value: String,
+    inputGroupSize: String,
+    icon: String,
+    type: String,
+    autocomplete: String,
+    autofocus: String,
+    tooltip: String,
+    feedbackText: String,
+    feedback: String,
+    lazy: String,
+    required: String,
+    readonly: Boolean,
+    disabled: String,
+    state: {
+      default: undefined,
+      type: Boolean
+    }
+  },
+  data() {
+    return {
+      isFocused: false,
+      isRequired: false,
+      isDisabled: false,
+      hadFocus: false
+    }
+  },
+  beforeMount() {
+    this.isFocused = common.toBoolean(this.autofocus);
+    this.isRequired = common.toBoolean(this.required);
+    this.isDisabled = common.toBoolean(this.disabled);
+  },
+  computed: {
+    innerValue: {
+      get: function() {
+        if (this.value && this.value.length > 0) {
+          //
+        }
+        return this.value;
+      },
+      set: function(newValue) {
+        return newValue;
+      }
+    },
+    inputListeners: function() {
+      const vm = this;
+      return Object.assign({},
+        this.$listeners,
+        {
+          input: function(event) {
+            vm.$emit('input', event);
+          }
+        }
+      )
+    },
+    inputClasses: function() {
+      return (this.isRequired) ? "required" : null;
+    },
+    labelClasses: function() {
+      return (this.isRequired) ? "required" : null;
+    }
+  },
+  methods: {
+    feedbackState: function() {
+      if (this.isDisabled && !this.isRequired) {
+        return undefined;
+      }
+      if (this.state !== undefined) {
+        return this.state;
+      }
+      if (common.toBoolean(this.lazy) && common.toBoolean(this.required)) {
+        if (this.value && this.value.length > 0) {
+          return true;
+        } else if (this.hadFocus) {
+          return false;
+        } else {
+          return undefined;
+        }
+      }
+      if (this.value && this.value.length > 0) {
+        return true;
+      } else {
+        return undefined;
+      }
+    }
+  }
+}
+</script>
---
+++ src/views/portfolio/vulnerabilities/SelectCweModal.vue
@@ -0,0 +1,75 @@
+<template>
+  <b-modal id="selectCweModal" size="md" hide-header-close :title="$t('message.select_cwe')">
+    <bootstrap-table
+      ref="table"
+      :columns="columns"
+      :data="data"
+      :options="options">
+    </bootstrap-table>
+    <template v-slot:modal-footer="{ cancel }">
+      <b-button size="md" variant="secondary" @click="cancel()">{{ $t('message.cancel') }}</b-button>
+      <b-button size="md" variant="primary" @click="$emit('selection', $refs.table.getSelections())">{{ $t('message.select') }}</b-button>
+    </template>
+  </b-modal>
+</template>
+
+<script>
+import xssFilters from "xss-filters";
+import permissionsMixin from "../../../mixins/permissionsMixin";
+import common from "../../../shared/common";
+
+export default {
+  mixins: [permissionsMixin],
+  data() {
+    return {
+      labelIcon: {
+        dataOn: '\u2713',
+        dataOff: '\u2715'
+      },
+      columns: [
+        {
+          field: "state",
+          checkbox: true,
+          align: "center"
+        },
+        {
+          title: this.$t('message.cwe_id'),
+          field: "cweId",
+          sortable: true,
+          formatter(value, row, index) {
+            return xssFilters.inHTMLData(common.valueWithDefault(value, ""));
+          }
+        },
+        {
+          title: this.$t('message.name'),
+          field: "name",
+          sortable: false,
+          formatter(value, row, index) {
+            return xssFilters.inHTMLData(common.valueWithDefault(value, ""));
+          }
+        }
+      ],
+      data: [],
+      options: {
+        search: true,
+        showColumns: true,
+        showRefresh: true,
+        pagination: true,
+        silentSort: false,
+        sidePagination: 'server',
+        queryParamsType: 'pageSize',
+        pageList: '[5, 10, 25, 50, 100]',
+        pageSize: 5,
+        icons: {
+          refresh: 'fa-refresh'
+        },
+        responseHandler: function (res, xhr) {
+          res.total = xhr.getResponseHeader("X-Total-Count");
+          return res;
+        },
+        url: `${this.$api.BASE_URL}/${this.$api.URL_CWE}`
+      }
+    };
+  }
+}
+</script>
