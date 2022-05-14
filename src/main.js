4cea46ffd504a18538333f82010c8e0adfb55c31
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-05-14 11:36:07 -0400

    feat: ability to enable xhr creds when cross-site
    Signed-off-by: Mohamed Nur <mohamed.nur@cds-snc.ca>

--- src/main.js
+++ src/main.js
@@ -44,6 +44,10 @@ axios.get(contextPath + "/static/config.json").then(response => {
   } else {
     Vue.prototype.$api.BASE_URL = contextPath;
   }
+
+  // XHR cross-site cookie credentials
+  Vue.prototype.$api.WITH_CREDENTIALS = response.data.API_WITH_CREDENTIALS;
+
   // OpenID Connect
   Vue.prototype.$oidc.ISSUER = response.data.OIDC_ISSUER;
   Vue.prototype.$oidc.CLIENT_ID = response.data.OIDC_CLIENT_ID;
