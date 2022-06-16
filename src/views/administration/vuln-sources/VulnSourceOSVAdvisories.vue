7a0aa7b0d070b9eefba04b8432fa2c51207e72e6
Sahiba Mittal <sahibamittal98@gmail.com>
2022-06-16 12:13:32 +0100

    fix button disabled
    Signed-off-by: Sahiba Mittal <sahibamittal98@gmail.com>

--- src/views/administration/vuln-sources/VulnSourceOSVAdvisories.vue
+++ src/views/administration/vuln-sources/VulnSourceOSVAdvisories.vue
@@ -3,7 +3,6 @@
     <b-card-body>
       <hr/>
       <c-switch
-        :disabled="!this.vulnsourceEnabled"
         color="primary"
         id="vulnsourceEnabled"
         label
@@ -16,7 +15,6 @@
     </b-card-body>
     <b-card-footer>
       <b-button
-        :disabled="this.vulnsourceEnabled"
         @click="saveChanges"
         class="px-4"
         variant="outline-primary">
@@ -42,7 +40,6 @@ export default {
   data() {
     return {
       vulnsourceEnabled: false,
-      apitoken: '',
       labelIcon: {
         dataOn: '\u2713',
         dataOff: '\u2715'
