42bf763f5d4a42bbbe77403cafeccae5da03dae0
Sahiba Mittal <sahibamittal98@gmail.com>
2022-10-19 15:09:21 +0100

    undo commit
    Signed-off-by: Sahiba Mittal <sahibamittal98@gmail.com>

--- src/views/administration/notifications/Alerts.vue
+++ src/views/administration/notifications/Alerts.vue
@@ -155,10 +155,6 @@
                         <actionable-list-group-item :add-icon="true" v-on:actionClicked="$root.$emit('bv::show::modal', 'selectProjectModal')"/>
                       </div>
                     </b-form-group>
-                  <div v-if="limitToVisible === true">
-                      <c-switch id="isNotifyChildrenEnabled" color="primary" v-model="notifyChildren" label v-bind="labelIcon"/>
-                      {{ $t('admin.include_children') }}
-                  </div>
                   </b-col>
                   <b-col sm="6">
                     <b-form-group id="fieldset-5" :label="this.$t('admin.scope')" label-for="input-5">
@@ -213,7 +209,6 @@
                   uuid: row.uuid,
                   name: row.name,
                   enabled: row.enabled,
-                  notifyChildren: row.notifyChildren,
                   publisherClass: row.publisher.publisherClass,
                   notificationLevel: row.notificationLevel,
                   destination: this.parseDestination(row),
@@ -233,9 +228,6 @@
               },
               watch: {
                 enabled() {
-                  this.updateNotificationRule();
-                },
-                notifyChildren() {
                   this.updateNotificationRule();
                 },
                 notifyOn() {
@@ -268,7 +260,6 @@
                     uuid: this.uuid,
                     name: this.name,
                     enabled: this.enabled,
-                    notifyChildren: this.notifyChildren,
                     notificationLevel: this.notificationLevel,
                     publisherConfig: JSON.stringify({ destination: this.destination }),
                     notifyOn: this.notifyOn
