b7a9317788abef0e05862ff6c553f94e12f48c2f
nscuro <nscuro@protonmail.com>
2022-10-22 21:25:55 +0200

    Remove dependency on vue-showdown
    Depend on Showdown directly instead, cutting out the middleman.
    
    Additionally:
    
    * Escape HTML outside of Markdown code blocks, in order to prevent unintended rendering of HTML elements (e.g. the `textarea` in CVE-2022-25869)
    * Always run Showdown output through DOMPurify
    
    Signed-off-by: nscuro <nscuro@protonmail.com>

---
+++ src/views/components/Showdown.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-html="outputHTML"></div>
+</template>
+
+<script>
+import DOMPurify from "dompurify";
+import showdown from "showdown";
+import * as showdownHtmlEscape from "showdown-htmlescape";
+
+export default {
+  name: "Showdown",
+  props: {
+    markdown: String,
+    allowHtml: {
+      type: Boolean,
+      default: false
+    },
+    flavor: {
+      type: String,
+      default: "github"
+    }
+  },
+  computed: {
+    converter() {
+      const c = new showdown.Converter();
+      if (!this.allowHtml) {
+        // Escape HTML tags outside of code blocks (Showdown will encode HTML tags within
+        // code blocks automatically). This prevents unintended rendering of HTML elements,
+        // but is not a security mechanism. Purely visual.
+        c.addExtension(showdownHtmlEscape, "html-escape");
+      }
+      c.setFlavor(this.flavor)
+      return c;
+    },
+    outputHTML() {
+      const html = this.converter.makeHtml(this.markdown);
+      return DOMPurify.sanitize(html);
+    }
+  }
+}
+</script>
