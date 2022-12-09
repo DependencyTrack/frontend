0c1b1e11af73e2debe27f59b96cc1030db2b70d5
rbt-mm <113189967+rbt-mm@users.noreply.github.com>
2022-12-09 17:01:50 +0100

    Focus on components in dependency graph and hide other nodes (#336)
    * Add buttons to show component in dependency graph
    
    Added new buttons to `Component`, `ProjectComponents` and
    `ProjectPolicyViolations`.
    
    Button in `ProjectFindings` is not yet added.
    
    Buttons don't have functionality yet.
    
    Signed-off-by: RBickert <rbt@mm-software.com>
    
    * Add new query parameter for dependency graph
    
    Adds a new query parameter for a project view, to find every occurence
    of a component in the dependency graph and highlight it.
    
    Signed-off-by: RBickert <rbt@mm-software.com>
    
    * Add hyperlink to different component views
    
    Adds hyperlinks to components in different views:
    - Component
    - ProjectComponents
    - ProjectFindings
    - ProjectPolicyViolations
    - ProjectEpss
    
    Those hyperlinks redirect to the projects dependency graph with every
    path to every occurrence of the specified component being expanded and
    the corresponding node being highlighted.
    
    Signed-off-by: RBickert <rbt@mm-software.com>
    
    * Add toggle `Hide other components`
    
    Adds new toggle to only show every direct path to the searched
    dependency and hide all other dependencies.
    
    Changed hyperlinks in other views to always display, regardless of a
    projects direct dependencies.
    
    Signed-off-by: RBickert <rbt@mm-software.com>
    
    * Display info text when dependency not in graph
    
    Signed-off-by: RBickert <rbt@mm-software.com>
    
    * Show dependency graph using param instead of query
    
    Use `$route.param` instead of `$route.query` to redirect to dependency
    graph to make redirect transition smooth instead of reloading the page
    everytime.
    
    Fix tooltips not showing or still displaying after redirect.
    
    Signed-off-by: RBickert <rbt@mm-software.com>
    
    * Fix ProjectPolicyViolations and Project
    
    Signed-off-by: RBickert <rbt@mm-software.com>
    
    * Change url path to make it more clear
    
    Signed-off-by: RBickert <rbt@mm-software.com>
    
    * Don't show complete graph on component lookup
    
    Signed-off-by: RBickert <rbt@mm-software.com>
    
    Signed-off-by: RBickert <rbt@mm-software.com>

--- src/views/portfolio/projects/ProjectEpss.vue
+++ src/views/portfolio/projects/ProjectEpss.vue
@@ -29,6 +29,7 @@ import i18n from "../../../i18n";
 import permissionsMixin from "../../../mixins/permissionsMixin";
 import BootstrapToggle from 'vue-bootstrap-toggle';
 import ChartEpssVsCvss from "../../dashboard/ChartEpssVsCvss";
+import $ from "jquery";
 
 export default {
   props: {
@@ -52,9 +53,10 @@ export default {
           title: this.$t('message.component'),
           field: "component.name",
           sortable: true,
-          formatter(value, row, index) {
-            let url = xssFilters.uriInUnQuotedAttr("../components/" + row.component.uuid);
-            return `<a href="${url}">${xssFilters.inHTMLData(value)}</a>`;
+          formatter: (value, row, index) => {
+            let url = xssFilters.uriInUnQuotedAttr("../../../components/" + row.component.uuid);
+            let dependencyGraphUrl = xssFilters.uriInUnQuotedAttr("../../../projects/" + this.uuid + "/dependencyGraph/" + row.component.uuid)
+            return `<a href="${dependencyGraphUrl}"<i class="fa fa-sitemap" aria-hidden="true" style="float:right; padding-top: 4px; cursor:pointer" data-toggle="tooltip" data-placement="bottom" title="Show in dependency graph"></i></a> ` + `<a href="${url}">${xssFilters.inHTMLData(value)}</a>`;
           }
         },
         {
@@ -78,7 +80,7 @@ export default {
           field: "vulnerability.vulnId",
           sortable: true,
           formatter(value, row, index) {
-            let url = xssFilters.uriInUnQuotedAttr("../vulnerabilities/" + row.vulnerability.source + "/" + value);
+            let url = xssFilters.uriInUnQuotedAttr("../../../vulnerabilities/" + row.vulnerability.source + "/" + value);
             return common.formatSourceLabel(row.vulnerability.source) + ` <a href="${url}">${xssFilters.inHTMLData(value)}</a>`;
           }
         },
@@ -133,7 +135,8 @@ export default {
           res.total = xhr.getResponseHeader("X-Total-Count");
           return res;
         },
-        url: this.apiUrl()
+        url: this.apiUrl(),
+        onPostBody: this.initializeTooltips,
       }
     };
   },
@@ -156,7 +159,12 @@ export default {
     tableLoaded: function(data) {
       this.$emit('total', data.total);
       this.$refs.chartEpssVsCvss.render(data);
-    }
+    },
+    initializeTooltips: function () {
+      $('[data-toggle="tooltip"]').tooltip({
+        trigger: "hover"
+      });
+    },
   },
   watch:{
     showSuppressedFindings() {
