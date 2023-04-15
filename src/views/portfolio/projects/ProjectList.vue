7be37423d2b75d049e80efe2194400bef1aace32
Valentijn Scholten <valentijnscholten@gmail.com>
2023-04-15 17:34:32 +0200

    add show more button
    Signed-off-by: Valentijn Scholten <valentijnscholten@gmail.com>

--- src/views/portfolio/projects/ProjectList.vue
+++ src/views/portfolio/projects/ProjectList.vue
@@ -188,7 +188,7 @@
               let tag_string = row.tags.slice(0, 2)?.map(tag => common.formatProjectTagLabel(tag)).join(', ') || '';
               if (row.tags.length > 2) {
                 tag_string += `, <span class="d-none">`
-                  tag_string += row.tags.slice(1)?.map(tag => common.formatProjectTagLabel(tag)).join(', ');
+                  tag_string += row.tags.slice(2)?.map(tag => common.formatProjectTagLabel(tag)).join(', ');
                   tag_string += `</span>`
                   tag_string += `<a href="#" title="show all tags" class="badge badge-tag" onclick="this.previousElementSibling.classList.toggle('d-none')">…</a>`
               }
