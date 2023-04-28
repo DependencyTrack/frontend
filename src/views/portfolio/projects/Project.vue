a3d318ae1b68922aaed75313d3b0de3626ef9823
Florian Heubeck <heubeck@mediamarktsaturn.com>
2023-04-28 10:54:29 +0200

    #484: Add external references to project page
    Signed-off-by: Florian Heubeck <heubeck@mediamarktsaturn.com>

--- src/views/portfolio/projects/Project.vue
+++ src/views/portfolio/projects/Project.vue
@@ -92,7 +92,30 @@
         </b-row>
       </b-card-body>
       <div id="project-info-footer" slot="footer">
-        <b-link class="font-weight-bold font-xs btn-block text-muted" @click="initializeProjectDetailsModal">{{ $t('message.view_details') }} <i class="fa fa-angle-right float-right font-lg"></i></b-link>
+        <b-row>
+          <b-col>
+            <b-link class="font-weight-bold font-xs btn-block text-muted" @click="initializeProjectDetailsModal">{{ $t('message.view_details') }} <i class="fa fa-angle-right float-right font-lg"></i></b-link>
+          </b-col>
+          <b-col v-if="project.externalReferences" md="auto">
+            <b-row class="d-none d-md-flex float-right">
+              <div>
+                <span><i class="fa fa-link text-muted" v-b-tooltip.hover="{title: $t('message.external_references')}"></i></span>
+                <ol style="display: inline-block; margin: 0; list-style-type: none; padding-inline-start: 0">
+                  <li class="dropdown">
+                    <a href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-caret-down" aria-hidden="true" style="padding-left:10px; padding-right:10px; padding-top:3px; padding-bottom:3px;"></i></a>
+                    <ul class="dropdown-menu">
+                      <span v-for="r in project.externalReferences" :key="r.url">
+                        <b-dropdown-item target="_blank" :href="r.url" v-b-tooltip.hover="{placement: 'left', title: r.url}">
+                          <i class="fa fa-external-link text-muted"></i>&nbsp;<span class="text-capitalize">{{ r.type }}</span>
+                        </b-dropdown-item>
+                      </span>
+                    </ul>
+                  </li>
+                </ol>
+              </div>
+            </b-row>
+          </b-col>
+      </b-row>
       </div>
     </b-card>
     <b-tabs class="body-bg-color" style="border-left: 0; border-right:0; border-top:0 ">
