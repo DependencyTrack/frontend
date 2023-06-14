6e63c24c0eab5e7237fce4548d1663bfce323879
Nicholas Castelli <nick.v.castelli@gmail.com>
2023-06-14 11:57:51 -0500

    Adding dependancies and functionality for table export
    Signed-off-by: Nicholas Castelli <nick.v.castelli@gmail.com>

--- src/plugins/table.js
+++ src/plugins/table.js
@@ -1,11 +1,16 @@
+import 'file-saver/dist/FileSaver.min.js'
+import 'tableexport.jquery.plugin/libs/js-xlsx/xlsx.core.min.js'
 import 'bootstrap-table/dist/bootstrap-table.min.css'
 
 import './jquery.js'
 import Vue from 'vue'
 import 'bootstrap'
 import 'jquery-treegrid/js/jquery.treegrid.min.js'
+import 'tableexport.jquery.plugin/tableExport.min.js'
 import 'bootstrap-table/dist/bootstrap-table.js'
 import 'bootstrap-table/dist/extensions/treegrid/bootstrap-table-treegrid.min.js'
+import 'bootstrap-table/dist/extensions/export/bootstrap-table-export.js'
 import BootstrapTable from 'bootstrap-table/dist/bootstrap-table-vue.esm.js'
+
 
 Vue.component('BootstrapTable', BootstrapTable);
