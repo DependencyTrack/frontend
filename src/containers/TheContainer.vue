3951dd08cf9b589c4db9f487ab1018b80ad0ea88
Walter de Boer <walterdeboer@dbso.nl>
2023-07-09 08:24:57 +0200

    Breadcrumbs back to TheContainer, restored SNASHOT Modal
    Signed-off-by: Walter de Boer <walterdeboer@dbso.nl>

--- src/containers/TheContainer.vue
+++ src/containers/TheContainer.vue
@@ -5,6 +5,7 @@
       <TheHeader/>
       <div class="c-body">
         <main class="c-main">
+          <CBreadcrumb class="mt-1 mb-4" :items="breadcrumbItems"/>
           <CContainer fluid>
               <router-view :key="$route.path"></router-view>
           </CContainer>
@@ -19,6 +20,7 @@
 import TheSidebar from './TheSidebar'
 import TheHeader from './TheHeader'
 import TheFooter from './TheFooter'
+import EventBus from '../shared/eventbus';
 
 export default {
   name: 'TheContainer',
@@ -26,6 +28,65 @@ export default {
     TheSidebar,
     TheHeader,
     TheFooter
+  },
+  data () {
+    return {
+      breadcrumbs: [],
+    }
+  },
+  methods: {
+    generateCBreadcrumbs: function (crumbName, subSectionName, subSectionUuid, subSectionLabel) {
+      let sectionLabel = this.$t(this.$route.meta.i18n);
+      let sectionPath = this.$route.meta.sectionPath;
+      if (crumbName && subSectionName && subSectionUuid && subSectionLabel) {
+        return [
+          { href: '/dashboard', text: this.$t('message.home') },
+          { href: sectionPath, text: sectionLabel },
+          { text: subSectionLabel, href: sectionPath + '/' + subSectionUuid },
+          { text: crumbName, active: true }
+        ];
+      } else if (crumbName) {
+        return [
+          { href: '/dashboard', text: this.$t('message.home') },
+          { href: sectionPath, text: sectionLabel },
+          { text: crumbName, active: true }
+        ];
+      } else {
+        return [
+          { href: '/dashboard', text: this.$t('message.home') },
+          { href: sectionPath, text: sectionLabel }
+        ];
+      }
+    }
+  },
+  mounted() {
+    if (this.$dtrack && this.$dtrack.version.includes("SNAPSHOT")) {
+      this.$root.$emit('bv::show::modal', 'snapshotModal');
+    }
+  },
+  computed: {
+    name () {
+      return this.$route.name
+    },
+    breadcrumbItems () {
+      if (this.breadcrumbs.length === 0) {
+        return this.generateCBreadcrumbs();
+      } else {
+        return this.breadcrumbs;
+      }
+    }
+  },
+  created() {
+    EventBus.$on('crumble', () => {
+      this.breadcrumbs = this.breadcrumbs.slice(0,-1);
+    });
+    EventBus.$on('addCrumb', (crumb, subSectionName, subSectionUuid, subsectionLabel) => {
+      if (crumb) {
+        this.breadcrumbs = this.generateCBreadcrumbs(crumb, subSectionName, subSectionUuid, subsectionLabel);
+      } else {
+        this.breadcrumbs = [];
+      }
+    });
   }
 }
 </script>
--- src/containers/TheHeader.vue
+++ src/containers/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <CHeader fixed with-subheader colorScheme="dark">
+  <CHeader fixed colorScheme="dark">
     <CToggler
       in-header
       class="ml-3 d-lg-none"
@@ -18,73 +18,16 @@
     <CHeaderNav class="ml-0">
       <TheHeaderDropdownAccnt/>
     </CHeaderNav>
-    <CSubheader class="px-3 pb-0 mb-0">
-      <CBreadcrumb class="my-0 pb-0" :items="breadcrumbItems"/>
-    </CSubheader>
   </CHeader>
 </template>
 
 <script>
 import TheHeaderDropdownAccnt from './TheHeaderDropdownAccnt'
-import EventBus from '../shared/eventbus';
 
 export default {
   name: 'TheHeader',
   components: {
     TheHeaderDropdownAccnt
-  },
-  data () {
-    return {
-      breadcrumbs: [],
-    }
-  },
-  methods: {
-    generateCBreadcrumbs: function (crumbName, subSectionName, subSectionUuid, subSectionLabel) {
-      let sectionLabel = this.$t(this.$route.meta.i18n);
-      let sectionPath = this.$route.meta.sectionPath;
-      if (crumbName && subSectionName && subSectionUuid && subSectionLabel) {
-        return [
-          { href: '/dashboard', text: this.$t('message.home') },
-          { href: sectionPath, text: sectionLabel },
-          { text: subSectionLabel, href: sectionPath + '/' + subSectionUuid },
-          { text: crumbName, active: true }
-        ];
-      } else if (crumbName) {
-        return [
-          { href: '/dashboard', text: this.$t('message.home') },
-          { href: sectionPath, text: sectionLabel },
-          { text: crumbName, active: true }
-        ];
-      } else {
-        return [
-          { href: '/dashboard', text: this.$t('message.home') },
-          { href: sectionPath, text: sectionLabel }
-        ];
-      }
-    }
-  },
-  computed: {
-    breadcrumbItems () {
-      if (this.breadcrumbs.length === 0) {
-        return this.generateCBreadcrumbs();
-      } else {
-        return this.breadcrumbs;
-      }
-    }
-  },
-  created() {
-    EventBus.$on('crumble', () => {
-      // Don't update breadcrumbs twice, only once on addCrumb:
-      // this.breadcrumbs = this.breadcrumbs.slice(0,-1);
-    });
-    EventBus.$on('addCrumb', (crumb, subSectionName, subSectionUuid, subsectionLabel) => {
-      // console.log('addCrumb');
-      if (crumb) {
-        this.breadcrumbs = this.generateCBreadcrumbs(crumb, subSectionName, subSectionUuid, subsectionLabel);
-      } else {
-        this.breadcrumbs = [];
-      }
-    });
   }
 }
 </script>
