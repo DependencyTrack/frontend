2a46ef17e3d64c52e7c6745329092ac2c45665f4
RBickert <rbt@mm-software.com>
2023-08-07 17:42:48 +0200

    Go to expanded finding from "Affected Projects"
    Clicking on a project from the "Affected Projects"-tab of a
    vulnerability now opens the project's "Findings"-tab, showing only
    the expanded vulnerability
    
    Signed-off-by: RBickert <rbt@mm-software.com>

--- src/views/portfolio/projects/ProjectFindings.vue
+++ src/views/portfolio/projects/ProjectFindings.vue
@@ -216,6 +216,12 @@ import common from "../../../shared/common";
             formatter(value, row, index) {
               return value === true ? '<i class="fa fa-check-square-o" />' : "";
             },
+          },
+          {
+            title: this.$t('message.matrix'),
+            field: "matrix",
+            sortable: true,
+            visible: false
           }
         ],
         data: [],
@@ -233,6 +239,7 @@ import common from "../../../shared/common";
           pageSize: (localStorage && localStorage.getItem("ProjectFindingsPageSize") !== null) ? Number(localStorage.getItem("ProjectFindingsPageSize")) : 10,
           sortName: (localStorage && localStorage.getItem("ProjectFindingsSortName") !== null) ? localStorage.getItem("ProjectFindingsSortName") : undefined,
           sortOrder: (localStorage && localStorage.getItem("ProjectFindingsSortOrder") !== null) ? localStorage.getItem("ProjectFindingsSortOrder") : undefined,
+          searchText: (this.$route.params.affectedComponent && this.$route.params.vulnerability) ? this.$route.params.uuid + ":" + this.$route.params.affectedComponent + ":" + this.$route.params.vulnerability : undefined,
           icons: {
             detailOpen: 'fa-fw fa-angle-right',
             detailClose: 'fa-fw fa-angle-down',
@@ -558,6 +565,9 @@ import common from "../../../shared/common";
       tableLoaded: function(data) {
         loadUserPreferencesForBootstrapTable(this, "ProjectFindings", this.$refs.table.columns);
         this.$emit('total', data.total);
+        if (this.$route.params.affectedComponent && this.$route.params.vulnerability) {
+          this.$refs.table.expandRow(0);
+        }
       },
       initializeTooltips: function () {
         $('[data-toggle="tooltip"]').tooltip({
--- src/views/portfolio/vulnerabilities/AffectedProjects.vue
+++ src/views/portfolio/vulnerabilities/AffectedProjects.vue
@@ -16,7 +16,8 @@
     mixins: [permissionsMixin],
     props: {
       source: String,
-      vulnId: String
+      vulnId: String,
+      vulnerability: String
     },
     data() {
       return {
@@ -26,7 +27,7 @@
             field: "name",
             sortable: true,
             formatter: (value, row, index) => {
-              let url = xssFilters.uriInUnQuotedAttr("../../../projects/" + row.uuid);
+              let url = xssFilters.uriInUnQuotedAttr("../../../projects/" + row.uuid + "/findings/" + row.affectedComponent + "/" + this.vulnerability);
               let dependencyGraphUrl = xssFilters.uriInUnQuotedAttr("../../../projects/" + row.uuid + "/dependencyGraph/" + row.affectedComponent)
               return row.directDependencies ? `<a href="${dependencyGraphUrl}"<i class="fa fa-sitemap" aria-hidden="true" style="float:right; padding-top: 4px; cursor:pointer" data-toggle="tooltip" data-placement="bottom" title="Show in dependency graph"></i></a> ` + `<a href="${url}">${xssFilters.inHTMLData(value)}</a>` : `<a href="${url}">${xssFilters.inHTMLData(value)}</a>`;
             }
