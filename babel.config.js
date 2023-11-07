0282da96845f25e2870bc58839482764c4b5cdf5
Sahiba Mittal <sahiba.mittal@citi.com>
2023-11-07 14:26:40 +0000

    merge from upstream

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-  plugins: [
-    '@babel/plugin-proposal-optional-chaining'
-  ],
   presets: [
     ['@vue/babel-preset-jsx'],
     ['@babel/preset-env', {
       'useBuiltIns': 'entry',
-      'corejs': '3.30'
+      'corejs': '3.33'
     }]
   ]
 }
