275c5437ae54859da1285b63161fa62bada8d8f3
Adam Setch <adam.setch@outlook.com>
2024-01-30 07:10:11 -0500

    feat(dashboard): add policy violations by state and classification cards
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/views/portfolio/projects/ServiceDashboard.vue
+++ src/views/portfolio/projects/ServiceDashboard.vue
@@ -78,7 +78,7 @@
             <b-col sm="7" class="d-none d-md-block">
             </b-col>
           </b-row>
-          <chart-policy-violations ref="chartPolicyViolations" chartId="chartPolicyViolations" class="chart-wrapper" style="height:200px;margin-top:40px;" :height="200"></chart-policy-violations>
+          <chart-policy-violations-state ref="chartPolicyViolationsState" chartId="chartPolicyViolationsState" class="chart-wrapper" style="height:200px;margin-top:40px;" :height="200"></chart-policy-violations-state>
         </b-card>
       </b-col>
       <b-col sm="6">
@@ -106,7 +106,7 @@ import { Callout } from '@coreui/vue'
 import ChartAuditingProgress from "../../dashboard/ChartAuditingProgress";
 import ChartComponentVulnerabilities from "../../dashboard/ChartComponentVulnerabilities";
 import ChartPortfolioVulnerabilities from '../../dashboard/ChartPortfolioVulnerabilities';
-import ChartPolicyViolations from "@/views/dashboard/ChartPolicyViolations";
+import ChartPolicyViolationsState from "@/views/dashboard/ChartPolicyViolationsState";
 import ChartPolicyViolationBreakdown from '@/views/dashboard/ChartPolicyViolationBreakdown';
 
 export default {
@@ -115,7 +115,7 @@ export default {
     ChartAuditingProgress,
     ChartComponentVulnerabilities,
     ChartPortfolioVulnerabilities,
-    ChartPolicyViolations,
+    ChartPolicyViolationsState,
     ChartPolicyViolationBreakdown,
     Callout,
 
@@ -184,7 +184,7 @@ export default {
     let url = `${this.$api.BASE_URL}/${this.$api.URL_METRICS}/service/${uuid}/days/${daysBack}`;
     this.axios.get(url).then((response) => {
       this.$refs.chartServiceVulnerabilities.render(response.data);
-      this.$refs.chartPolicyViolations.render(response.data);
+      this.$refs.chartPolicyViolationsState.render(response.data);
       this.$refs.chartPolicyViolationBreakdown.render(response.data);
       this.extractStats(response.data);
     });
