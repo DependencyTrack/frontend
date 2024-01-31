6eebb1c8e1fcd9b4905c0406ce8ca6ab5229f68c
Adam Setch <adam.setch@outlook.com>
2024-01-31 15:19:49 -0500

    feat(vulnerabilities): enhance Vulnerabilities UI to be able to toggle affected projects by active/inactive
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/views/portfolio/vulnerabilities/AffectedProjects.vue
+++ src/views/portfolio/vulnerabilities/AffectedProjects.vue
@@ -1,19 +1,33 @@
 <template>
+  <div>
+    <div id="projectsToolbar" class="bs-table-custom-toolbar">
+      <c-switch style="margin-left:1rem; margin-right:.5rem" id="showInactiveProjects" color="primary" v-model="showInactiveProjects" label v-bind="labelIcon" /><span class="text-muted">{{ $t('message.show_inactive_projects') }}</span>
+    </div>
     <bootstrap-table
       ref="table"
       :columns="columns"
       :data="data"
       :options="options"
-      @on-load-success="tableLoaded">
+      @on-load-success="tableLoaded"
+      @on-post-body="onPostBody">
     </bootstrap-table>
+  </div>
 </template>
 
 <script>
   import xssFilters from "xss-filters";
   import permissionsMixin from "../../../mixins/permissionsMixin";
+  import { Switch as cSwitch } from '@coreui/vue';
+
 
   export default {
     mixins: [permissionsMixin],
+    components: {
+      cSwitch
+    },
+    beforeCreate() {
+      this.showInactiveProjects = (localStorage && localStorage.getItem("AffectedProjectListShowInactiveProjects") !== null) ? (localStorage.getItem("AffectedProjectListShowInactiveProjects") === "true") : false;
+    },
     props: {
       source: String,
       vulnId: String,
@@ -21,6 +35,11 @@
     },
     data() {
       return {
+        showInactiveProjects: this.showInactiveProjects,
+        labelIcon: {
+          dataOn: '\u2713',
+          dataOff: '\u2715'
+        },
         columns: [
           {
             title: this.$t('message.name'),
@@ -45,6 +64,15 @@
             title: this.$t('message.version'),
             field: "version",
             sortable: true,
+          },
+          {
+            title: this.$t('message.active'),
+            field: "active",
+            formatter(value, row, index) {
+              return value === true ? '<i class="fa fa-check-square-o" />' : "";
+            },
+            align: "center",
+            sortable: true
           }
         ],
         data: [],
@@ -61,14 +89,43 @@
           icons: {
             refresh: 'fa-refresh'
           },
-          url: `${this.$api.BASE_URL}/${this.$api.URL_VULNERABILITY}/source/${this.source}/vuln/${this.vulnId}/projects`
+          url: this.apiUrl()
         }
       };
     },
     methods: {
+      apiUrl: function () {
+        let url = `${this.$api.BASE_URL}/${this.$api.URL_VULNERABILITY}/source/${this.source}/vuln/${this.vulnId}/projects`;
+        if (this.showInactiveProjects === undefined) {
+          url += "?excludeInactive=true";
+        } else {
+          url += "?excludeInactive=" + !this.showInactiveProjects;
+        }
+        return url;
+      },
       tableLoaded: function(array) {
         this.$emit('total', array.length);
+      },
+      refreshTable: function () {
+        this.$refs.table.refresh({
+          url: this.apiUrl(),
+          silent: true,
+          pageNumber: 1
+        });
+      },
+      onPostBody: function () {
+        this.$refs.table.hideLoading();
       }
+    },
+    watch: {
+      showInactiveProjects() {
+        if (localStorage) {
+          localStorage.setItem("AffectedProjectListShowInactiveProjects", this.showInactiveProjects.toString());
+        }
+        this.$refs.table.showLoading();
+        this.currentPage = 1;
+        this.refreshTable();
+      },
     }
   };
 </script>
