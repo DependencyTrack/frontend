05228e16c8157a2740bc85fe7e2769ead9843bf4
Adam Setch <adam.setch@outlook.com>
2024-02-26 15:41:23 -0500

    feat: add eslint and prettier
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:vue/essential',
-    // 'plugin:prettier/recommended',
+    'plugin:prettier/recommended',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
