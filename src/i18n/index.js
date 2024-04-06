432772ee46ecec091119ff1886b063e203058b70
Marlon Pina Tojal <marlont@backbase.com>
2024-04-06 12:50:04 +0200

    add support from localization based on browser
    Signed-off-by: Marlon Pina Tojal <marlont@backbase.com>

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -21,7 +21,7 @@ function loadLocaleMessages() {
 }
 
 const i18n = new VueI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || 'en',
+  locale: navigator.language || navigator.userLanguage,
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
   messages: loadLocaleMessages(),
 });
