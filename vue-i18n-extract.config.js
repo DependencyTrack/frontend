74ca02787081c9781df2e6b2bfbbf7482b0a59c2
nscuro <nscuro@protonmail.com>
2024-04-22 22:59:16 +0200

    Move vue-i18n-extract flags to config file; Add excludes for `language.*`
    Signed-off-by: nscuro <nscuro@protonmail.com>

---
+++ vue-i18n-extract.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  exclude: [
+    "language.de",
+    "language.en",
+    "language.es",
+    "language.fr",
+    "language.hi",
+    "language.it",
+    "language.ja",
+    "language.pl",
+    "language.pt",
+    "language.pt-BR",
+    "language.ru",
+    "language.zh",
+    "validation.confirmed",
+    "validation.max_value",
+    "validation.min_value",
+    "validation.required",
+  ],
+  languageFiles: "src/i18n/locales/*.json",
+  vueFiles: "src/**/*.?(vue|js)"
+}
