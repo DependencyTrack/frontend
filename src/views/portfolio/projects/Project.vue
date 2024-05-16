f335cebe1a0c4ec5af1ed782a50d03890eecfe77
Aravind Parappil <aravindparappil@gmail.com>
2024-05-15 23:50:29 -0400

    Show Badge For Inactive Projects
    In order to denote that a project is inactive, show a badge next to the project name/version
    in the Project details page
    
    Signed-off-by: Aravind Parappil <aravindparappil@gmail.com>

--- src/views/portfolio/projects/Project.vue
+++ src/views/portfolio/projects/Project.vue
@@ -52,7 +52,7 @@
 
                         <b-dropdown-group
                           v-if="inactiveProjectVersions.length > 0"
-                          header="Inactive Versions"
+                          :header="$t('message.inactive_versions')"
                         >
                           <span
                             v-for="projectVersion in inactiveProjectVersions"
@@ -72,6 +72,9 @@
                   </ol>
                   {{ project.version }}
                 </b-col>
+                <b-badge v-if="!this.project.active" :variant="'tab-warn'">
+                  {{ $t('message.inactive').toUpperCase() }}
+                </b-badge>
                 <b-col class="d-none d-md-flex">
                   <span
                     class="text-muted font-xs font-italic align-text-top text-truncate"
