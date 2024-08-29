16c893ab407d1b44354bebe65751fc5a699a5983
Sekwah <contact@sekwah.com>
2024-08-29 14:50:46 +0200

    wip: got the basic template working [skip ci]
    Signed-off-by: Sekwah <contact@sekwah.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,14 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
   root: true,
-  env: {
-    node: true,
-  },
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-  },
-  extends: [
+  'extends': [
+    'plugin:vue/vue3-essential',
     'eslint:recommended',
-    'plugin:vue/essential',
-    'plugin:prettier/recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
-};
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <div>This is just a test</div>
+  </main>
+</template>
