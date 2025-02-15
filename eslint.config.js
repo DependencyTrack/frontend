a9bad281ccd3bc71e0b935c9f78ab3e44d594c7e
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2025-02-15 12:32:48 +0100

    address some eslint errors
    Signed-off-by: Steffen Ohrendorf <steffen.ohrendorf@gmx.de>

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,19 @@ import recommendedConfig from 'eslint-plugin-prettier/recommended';
 export default [
   {
     name: 'app/files-to-lint',
-    files: ['src/**/*.{js,vue,md,yaml}'],
+    files: ['src/**/*.{js,jsx,vue,md,yaml}'],
+  },
+
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
   },
 
   recommendedConfig,
