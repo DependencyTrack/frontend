4ce08069f3b5b8a91f9d74bff4c7c539c9afe6b9
Anton Jäppinen Johansson <antja@westermo.com>
2026-01-15 12:24:05 +0100

    new AliasModal Created a modal to select alias for the vulnerability
    Signed-off-by: Anton Jäppinen Johansson <antja@westermo.com>

---
+++ src/views/portfolio/vulnerabilities/SelectAliasModal.vue
@@ -0,0 +1,88 @@
+<template>
+  <b-modal
+    id="selectAliasModal"
+    size="md"
+    hide-header-close
+    :title="$t('message.aliases')"
+  >
+    <bootstrap-table
+      ref="table"
+      :columns="columns"
+      :data="data"
+      :options="options"
+    >
+    </bootstrap-table>
+    <template v-slot:modal-footer="{ cancel }">
+      <b-button size="md" variant="secondary" @click="cancel()">{{
+        $t('message.cancel')
+      }}</b-button>
+      <b-button
+        size="md"
+        variant="primary"
+        @click="$emit('selection', $refs.table.getSelections())"
+        >{{ $t('message.select') }}</b-button
+      >
+    </template>
+  </b-modal>
+</template>
+
+<script>
+import xssFilters from 'xss-filters';
+import permissionsMixin from '../../../mixins/permissionsMixin';
+import common from '../../../shared/common';
+
+export default {
+  mixins: [permissionsMixin],
+  data() {
+    return {
+      labelIcon: {
+        dataOn: '\u2713',
+        dataOff: '\u2715',
+      },
+      columns: [
+        {
+          field: 'state',
+          checkbox: true,
+          align: 'center',
+        },
+        {
+          title: 'Source',
+          field: 'source',
+          sortable: false,
+          formatter(value, row, index) {
+            return xssFilters.inHTMLData(common.valueWithDefault(value, ''));
+          },
+        },
+        {
+          title: this.$t('message.name'),
+          field: 'vulnId',
+          sortable: true,
+          formatter(value, row, index) {
+            return xssFilters.inHTMLData(common.valueWithDefault(value, ''));
+          },
+        },
+      ],
+      data: [],
+      options: {
+        search: true,
+        showColumns: true,
+        showRefresh: true,
+        pagination: true,
+        silentSort: false,
+        sidePagination: 'server',
+        queryParamsType: 'pageSize',
+        pageList: '[10, 25, 50, 100]',
+        pageSize: 10,
+        icons: {
+          refresh: 'fa-refresh',
+        },
+        responseHandler: function (res, xhr) {
+          res.total = xhr.getResponseHeader('X-Total-Count');
+          return res;
+        },
+        url: `${this.$api.BASE_URL}/${this.$api.URL_VULNERABILITY}`,
+      },
+    };
+  },
+};
+</script>
